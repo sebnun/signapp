d9ab162dad70be0d4a09426c9ecfca41e6fdda5f
sebnun <sebnun@gmail.com>
2017-07-12 19:42:43 -0300

    deployed app to github

--- src/feedback.js
+++ src/feedback.js
@@ -12,6 +12,7 @@ function getFeedback(rawEmail) {
         return { type: 'error', message: `Email can't be empty!` };
 
     const pseudoEmail = rawEmail.toLowerCase();
+    // eslint-disable-next-line
     const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     //check if it's a proper email
